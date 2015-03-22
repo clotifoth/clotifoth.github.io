781aeb33a2ff2d4ca9dbd18def7576b02d367b47
Frederick Brunn <frederick.brunn@stonybrook.edu>
2015-03-21 20:59:52 -0400

    Back to status
    Testing mobile hovering will wait, website needs to be up now.

--- js/mobile.js
+++
@@ -1,9 +0,0 @@
-var p = document.getElementsByTagName('p');
-for(var i = 0; i < p.length; i++)
-{
-	p.onclick = function() {
-	 // Trigger the `hover` event on the paragraph
-		p.onhover.call(p);
-	};
-    console.log("Yey\n");
-}
